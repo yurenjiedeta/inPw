8b78a3b6f78fe478efb896f00a9955cf8ecc06a8
luyigen <luyigen@empowerwin.com>
2024-07-29 11:57:56 +0800

    lnnp

---
+++ fast think/lnmp/dbeaver-jdbc.md
@@ -0,0 +1,6 @@
+连接地址加参数
+
+```tex
+127.0.0.1:3306/testdb?allowPublicKeyRetrieval=true&useSSL=false
+```
+
---
+++ fast think/lnmp/lnmp/php-app/index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mysql";
+$username = "user";
+$password = "password";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
